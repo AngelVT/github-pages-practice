5d8272823a6ec30542e7ea3eb71a48c064139977
AngelVT <angelovelaz32@gmail.com>
2024-11-16 01:29:13 -0600

    removed unused function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,5 @@
-document.getElementsByTagName("html")[0].style.backgroundImage = `url("../img/background_${getRandomNumber()}.webp")`;
+document.getElementsByTagName("html")[0].style.backgroundImage = `url(../img/background_${getRandomNumber()}.webp)`;
 
 function getRandomNumber() {
     return Math.floor(Math.random() * 20) + 1;
 }
-
-changeBackGround();
