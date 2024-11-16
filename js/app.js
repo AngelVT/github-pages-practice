f5bbf0c70ea889dd67111a9fd8bcb412594e6f5a
AngelVT <angelovelaz32@gmail.com>
2024-11-16 01:25:11 -0600

    correcion de carga de recursos desde el html

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-document.getElementsByTagName("html")[0].style.backgroundImage = `url(/img/background_${getRandomNumber()}.webp)`;
+document.getElementsByTagName("html")[0].style.backgroundImage = `url("../img/background_${getRandomNumber()}.webp")`;
 
 function getRandomNumber() {
     return Math.floor(Math.random() * 20) + 1;
